c0ad60af86dc35e3e3ebe6893dae4d2f6c05bceb
Douglas Sigmon <douglas.sigmon@gmail.com>
2020-08-28 17:50:32 -0600

    user stories 1-3

--- src/makeChange/MakeChange.java
+++ src/makeChange/MakeChange.java
@@ -1,10 +1,36 @@
 package makeChange;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		double cost = costPrompt();
+		double moneyRecieved = MoneyTendered();
+		
+		if (cost == moneyRecieved) {
+			System.out.println("Thank you for exact change as there is a change shortage due to COVID19!!");
+		} else if (cost > moneyRecieved) {
+			System.err.println("Insuffecient Funds, the items cost: " + cost + " you gave " + moneyRecieved);
+		}
+		
 	}
+
+	private static double MoneyTendered() {
+		Scanner kb = new Scanner(System.in);
+		System.out.println("How much money did the customer give you?");
+		double moneyRecieved;
+		moneyRecieved = kb.nextDouble();	
+		return moneyRecieved;
+	}
+
+	private static double costPrompt() {
+		Scanner kb = new Scanner(System.in);
+		System.out.println("How much did this item cost?");
+		double cost;
+		cost = kb.nextDouble();
+		return cost;
+	}
+	
 
 }
